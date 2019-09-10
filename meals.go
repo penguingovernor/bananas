c4120ec2179dc6f7e052f456ebbd93ded7179819
Jorge Henriquez <penguingovernor@gmail.com>
2019-09-09 21:26:09 -0700

    Add meal constants

---
+++ meals.go
@@ -0,0 +1,15 @@
+package bananas
+
+// Meal represents all possible meals at UCSC.
+type Meal string
+
+const (
+	// Breakfast represents a breakfast meal.
+	Breakfast Meal = "Breakfast"
+	// Lunch represents a lunch meal.
+	Lunch Meal = "Lunch"
+	// Dinner represents a dinner meal.
+	Dinner Meal = "Dinner"
+	// LateNight represents a late night meal.
+	LateNight Meal = "Late Night"
+)
